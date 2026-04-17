4e7faa7f7db449524e7a21bf8f7ede8259cc33cf
Ignacio Gelado <[email protected]>
2026-04-18 00:51:05 +0200

    Tune logo height to 90px to fit Install button

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3690,7 +3690,7 @@ Widget loadLogo() {
             },
           );
           return Container(
-            constraints: BoxConstraints(maxWidth: 300, maxHeight: 120),
+            constraints: BoxConstraints(maxWidth: 300, maxHeight: 90),
             child: image,
           ).marginOnly(left: 12, right: 12, top: 12);
         }
