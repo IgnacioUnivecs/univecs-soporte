40be672c5e57836f6b46c98b8d43c6a9edcce1bb
Ignacio Gelado <[email protected]>
2026-04-17 23:32:02 +0200

    Restore hoverBorder constant accidentally removed

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -261,6 +261,7 @@ class MyTheme {
   static const Color cmIdColor = Color(0xFF21790B);
   static const Color dark = Colors.black87;
   static const Color button = Color(0xFF00ADD8);
+  static const Color hoverBorder = Color(0xFF999999);
 
   // ListTile
   static const ListTileThemeData listTileTheme = ListTileThemeData(
